63ac30de67f4b97368c731d97e4fe87ecefcbf47
sabbir1257 <masabbir1257@gmail.com>
2024-10-01 00:21:19 +0600

    (Recap) Calculate Rectangle area and display video  4.10 push

--- scripts/rectangle.js
+++ scripts/rectangle.js
@@ -1,4 +1,10 @@
 function calculateRectangleArea(){
-     console.log('claculate rect area');
+     // get lenght of the rectangle
+     const lenghtInput = document.getElementById('rectangle-lenght');
+     const lenghtText = lenghtInput.value;
+     const lenght = parseFloat(lenghtText);
+     console.log(lenght);
+
+     // get width of the rectangle
      
 }
