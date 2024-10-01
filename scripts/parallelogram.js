ac2a95d62bcda4bae28aa6ae9c156b60f54751cc
sabbir1257 <masabbir1257@gmail.com>
2024-10-01 14:21:34 +0600

    Extract common code into a function

--- scripts/parallelogram.js
+++ scripts/parallelogram.js
@@ -1,21 +1,31 @@
 function calculateParallelogramArea() {
-     //base input fild
+     // //base input fild
+    /*  const baseInput = document.getElementById('parallelogram-base');
+     const baseText = baseInput.value;
+     const base = parseFloat(baseText)
+     console.log('base input :', base); */
+
+     // // height input fild 
+    /*  const heightInput = document.getElementById('parallelogram-height');
+     const heightText = heightInput.value;
+     const height = parseFloat(heightText)
+     console.log(height); */
+
+     // // calculate area
+    /*  const area = base * height;
+     console.log('area of the parallelogram:', area); */
+     
+     // // display Parallelogram area
+    /*  const display = document.getElementById('parallelogram-area');
+     display.innerText = area; */
+
+     const base = getParallelogramBase();
+     console.log(base);
+}
+
+function getParallelogramBase(){
      const baseInput = document.getElementById('parallelogram-base');
      const baseText = baseInput.value;
      const base = parseFloat(baseText)
-     console.log('base input :', base);
-
-     // height input fild 
-     const heightInput = document.getElementById('parallelogram-height');
-     const heightText = heightInput.value;
-     const height = parseFloat(heightText)
-     console.log(height);
-
-     // calculate area
-     const area = base * height;
-     console.log('area of the parallelogram:', area);
-     
-     // display Parallelogram area
-     const display = document.getElementById('parallelogram-area');
-     display.innerText = area;
+     return(base)
 }
