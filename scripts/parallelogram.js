d569706ee78feeb199f1755d986c5ab1918a3f36
sabbir1257 <masabbir1257@gmail.com>
2024-10-01 14:03:49 +0600

    Parallelogram disagin and function complite

---
+++ scripts/parallelogram.js
@@ -0,0 +1,21 @@
+function calculateParallelogramArea() {
+     //base input fild
+     const baseInput = document.getElementById('parallelogram-base');
+     const baseText = baseInput.value;
+     const base = parseFloat(baseText)
+     console.log('base input :', base);
+
+     // height input fild 
+     const heightInput = document.getElementById('parallelogram-height');
+     const heightText = heightInput.value;
+     const height = parseFloat(heightText)
+     console.log(height);
+
+     // calculate area
+     const area = base * height;
+     console.log('area of the parallelogram:', area);
+     
+     // display Parallelogram area
+     const display = document.getElementById('parallelogram-area');
+     display.innerText = area;
+}
--- scripts/rectangle.js
+++ scripts/rectangle.js
@@ -6,5 +6,16 @@ function calculateRectangleArea(){
      console.log(lenght);
 
      // get width of the rectangle
+     const widthInput = document.getElementById('rectangle-width');
+     const widthText = widthInput.value;
+     const width  = parseFloat(widthText);
+     console.log(width);
      
+     // calculate rectaongle area
+     const area = lenght * width;
+     console.log('area of the rectangle:', area);
+
+     // display rectangle area
+     const rectangleAreaSpan = document.getElementById('rectangle-area');
+     rectangleAreaSpan.innerText = area;
 }
